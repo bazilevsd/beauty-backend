53e127cac6017f0284d6c3d33d9dc3f4284ddb17
bazilevsd <114525681+bazilevsd@users.noreply.github.com>
2023-07-13 19:11:57 -0700

    added trycatch

--- src/server.ts
+++ src/server.ts
@@ -12,46 +12,51 @@ import userRouter from "./routers/api/users";
 import appointmentDayRouter from "./routers/api/appointmentDay";
 import path from "path";
 import favicon from "express-favicon";
+try {
+  const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+  const app = express();
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(checkToken);
 
-const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(checkToken);
+  const corsOptions = {
+    origin: "*",
+  };
 
-const corsOptions = {
-  origin: "*",
-};
+  app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+  const db = mongoose.connection;
+  mongoose.connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions);
 
-const db = mongoose.connection;
-mongoose.connect(`${process.env.MONGO_URI}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-} as ConnectOptions);
+  db.once("open", () => {
+    console.log("connected to mongo");
+  });
 
-db.once("open", () => {
-  console.log("connected to mongo");
-});
+  app.use((req, res, next) => {
+    res.locals.data = {};
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.locals.data = {};
-  next();
-});
+  app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+  app.use(express.static(path.join(__dirname, "build")));
 
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
-app.use(express.static(path.join(__dirname, "build")));
+  app.use(checkToken);
+  app.use("/users", userRouter);
+  app.use("/setappointment", appointmentDayRouter);
 
-app.use(checkToken);
-app.use("/users", userRouter);
-app.use("/setappointment", appointmentDayRouter);
+  app.get("/", (req, res) => {
+    res.json({ message: "Service is alive!" });
+  });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Service is alive!" });
-});
-
-app.listen(PORT, () => {
-  console.log(chalk.blue(`Server is starting 🚀 on PORT: ${PORT}`));
-});
+  app.listen(PORT, () => {
+    console.log(chalk.blue(`Server is starting 🚀 on PORT: ${PORT}`));
+  });
+} catch (err) {
+  console.log(
+    `Application has crashed with following error ${JSON.stringify(err)}`
+  );
+}
